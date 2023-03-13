5fa4bc67758a61596290c05c830ae3a3dbfbe679
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-03-13 10:26:02 -0700

    Changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,10 +6,12 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: '{ Noah King }',
-  role: 'Full Stack Software Engineer',
+
+  // On this website, you will find a showcase of my software engineering skills, business acumen, and my passion for building scalable solutions. I am constantly learning and improving my technical skills, and I am excited to share my projects with prospective employers who are interested in my skills and experience. As you navigate through my portfolio website, you will find examples of my work, including projects that I have completed during my full stack software engineering course. You can also read about my experience in building relationships, overseeing projects, and creating a positive workplace culture, which are all critical skills that I bring to the table. I am excited to connect with prospective employers who are interested in my skills and experience. Thank you for visiting my portfolio website, and I look forward to hearing from you soon!
+  name: 'Noah King',
+  role: 'Jack of All Trades',
   description:
-  "I'm an Entreprenuer and aspring software developer who enjoy's building applications that help solve problems in the world.",
+  " I am a driven entrepreneur who recently sold my first company and began my transition into the technology industry. With a full stack software engineering course under my belt, I am eager to leverage my expertise in building relationships, overseeing projects, improving efficiency, and creating scalable operations to excel in a new career in the technology or Web3 space. ",
   resume: 'https://drive.google.com/uc?id=1l9yN4fsP4rkMl5sCDUqYe-fRZ5x5yWN9',
   social: {
     linkedin: 'https://www.linkedin.com/in/noah-b-king/',
@@ -20,10 +22,11 @@ const about = {
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
+ 
   {
     name: 'JobVerify',
     description:
-      "Website designed to streamline the hiring process by allowing current and past employees to request employement verifcation from companies, saving time and increasing credibility.",
+      "Website designed to streamline the hiring process by allowing current and past employees to request employement verifcation from companies, saving time and increasing credibility. (in progress)",
     stack: ['Rails', 'JavaScript', 'React'],
     sourceCode: 'https://github.com/NoahBroah/JobVerify',
     // livePreview: 'https://github.com',
